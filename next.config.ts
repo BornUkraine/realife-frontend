efc3fac4a51a8c14e5a07909bbaa5bc767f05091
BornUkraine <born8730@gmail.com>
2026-02-28 16:51:31 +0200

    chore: remove webpack config

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
-
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...(config.resolve.fallback || {}),
-      "@react-native-async-storage/async-storage": false,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
